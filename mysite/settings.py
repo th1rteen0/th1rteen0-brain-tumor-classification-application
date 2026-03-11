b18ea902719f12efa944d7d8a25f8f10fd95dfb6
aleya <rjd8wv@virginia.edu>
2026-03-11 04:19:57 -0400

    allow render as host

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SUPER_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['th1rteen0-brain-tumor-classification-vm7a.onrender.com', '127.0.0.1']
 
 
 # Application definition
